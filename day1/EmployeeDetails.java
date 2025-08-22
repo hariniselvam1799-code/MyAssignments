ccc30e4b04f07b6763ec0c66c0be554059ab54ab
hariniselvam1799-code <hariniselvam1799@gmail.com>
2025-08-22 21:52:59 +0530

    Add files via upload

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		String name="Harini Selvam";
+	    byte age=26;
+	    int salary=100000;
+	    char gender='F';
+	    boolean acceptTerms=true;
+	    float experience=5.5f;
+	    String address="No 3 5th street Sakthi Ganapathy Nagar TVT Ch-19";
+	    long mobileNumber=9776565398L;
+	    System.out.println(
+	    		"Employee Name:" +name + "\n" +
+	    		"Age:" +age + "\n" +
+	    		"Monthly Salary:" +salary + "\n" +
+	    		"Gender:" +gender + "\n" +
+	    		"Accept Terms(True/False):" +acceptTerms + "\n" +
+	    		"Experience:" +experience + "\n" +
+	    		"Address:" +address + "\n" +
+	    		"Mobile Number:" +mobileNumber
+	    		);
+	}
+
+}
